81a2a0c93bec075b5e495bf398bae393acaec46b
sohail60 <sohailatwork10@gmail.com>
2023-07-26 21:12:18 +0530

    modify cors

--- server/index.js
+++ server/index.js
@@ -10,9 +10,10 @@ const connectDB = require('./db/conn.js')
 const { authenticate, secretKey }  = require('./middleware/auth');
 
 const app = express();
-app.use(cors({
-  origin: 'https://e-kaksha.vercel.app/'
-}));
+// app.use(cors({
+//   origin: 'https://e-kaksha.vercel.app/'
+// }));
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
