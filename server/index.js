f1603daebcafb6c30f2fc20fa4eb01d52af5067f
sohail60 <sohailatwork10@gmail.com>
2023-07-26 19:16:11 +0530

    Add env file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+
 const adminRoute = require('./routes/admin.js')
 const userRoute = require('./routes/user.js')
+const connectDB = require('./db/conn.js')
 const { authenticate, secretKey }  = require('./middleware/auth');
 
 const app = express();
@@ -15,17 +17,6 @@ app.use(express.json());
 
 app.use('/admin', adminRoute);
 app.use('/users', userRoute);
-
-// MongoDB Connection
-async function connectDB() {
-  try {
-    const uri = 'mongodb+srv://sohailatwork10:sohail@cluster0.bnxf07q.mongodb.net/?retryWrites=true&w=majority';
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Database connected');
-  } catch (err) {
-    console.log('Error connecting to DB: ' + err);
-  }
-}
 
 connectDB();
 
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
-const secretKey = 'sohail'
+const dotenv = require('dotenv');
+dotenv.config();
+const secretKey = process.env.SECRET_KEY;
 
 async function authenticate(req, res, next) {
     // console.log(req.body);
