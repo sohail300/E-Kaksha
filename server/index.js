8aeaaddbca63ad115030008daf43b10e781b2242
sohail60 <sohailatwork10@gmail.com>
2023-07-26 21:20:14 +0530

    modify cors

--- server/index.js
+++ server/index.js
@@ -35,6 +35,6 @@ app.get('/profile', authenticate, (req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
