671c898ab2fba3e23ea16e4afc6ff8ca1db36077
sohail808 <sohailworks10@gmail.com>
2024-07-14 19:53:10 +0530

    urls changed

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://e-kaksha-9l1a.onrender.com";
-export const baseURL = "http://localhost:3000";
+const baseURL = "https://backend-e-kaksha.heysohail.me";
+// export const baseURL = "http://localhost:3000";
 
 export const api = axios.create({
   baseURL,
--- server/db/firebase.ts
+++ server/db/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getStorage, ref } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcKxfdGn6zlWBLNEN5LfaoVa9_hdQPIjU",
-  authDomain: "e-kaksha-2001.firebaseapp.com",
-  projectId: "e-kaksha-2001",
-  storageBucket: "e-kaksha-2001.appspot.com",
-  messagingSenderId: "196457869363",
-  appId: "1:196457869363:web:4257266483cca5208cb346",
-  measurementId: "G-VL1YZQ0X6Q",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
