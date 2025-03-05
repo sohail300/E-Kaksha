71662d6ff33522f250d22af5d88b876dcf3fcaf7
Md Sohail Ansari <sohailworks10@gmail.com>
2025-03-05 20:11:06 +0530

    admin config.ts

--- admin/src/utils/config.ts
+++ admin/src/utils/config.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const baseURL = "https://backend-e-kaksha.heysohail.me";
+// export const baseURL = "https://backend-e-kaksha.heysohail.me";
 // export const baseURL = "http://localhost:3000";
+export const baseURL = "https://e-kaksha-59dj.onrender.com";
 
 export const api = axios.create({
   baseURL,
